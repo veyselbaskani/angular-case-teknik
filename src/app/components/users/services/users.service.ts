26a0e1bee1bf65323e33624eac838b4e5bfe8980
Veysel Baskani <veyselbaskani@outlook.com>
2025-07-21 01:19:47 +0300

    Projects features

--- src/app/components/users/services/users.service.ts
+++ src/app/components/users/services/users.service.ts
@@ -2,20 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/users';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  private apiUrl = environment.backendUrl;
 
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    return this.http.get<User[]>(environment.backendUrl + '/users');
   }
 
   getUserById(id: number): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/${id}`);
+    return this.http.get<User>(`${environment.backendUrl + '/users'}/${id}`);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     baseUrl: 'http://localhost:4200',
     production: true,
-    backendUrl: 'http://172.20.24.102:9980/'
+    backendUrl: 'https://jsonplaceholder.typicode.com'
 };
